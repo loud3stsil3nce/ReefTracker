53bc66130ce796bcfce94eae67bc171530ed1614
loud3stsil3nce <rafiurrahman325@gmail.com>
2025-06-24 22:55:39 -0400

    fix

--- ReefTracker/myapp/urls.py
+++ ReefTracker/myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
     path('register', views.register, name='register'),  
     path('profile', views.profile, name='profile'),
     path('calculators', views.calculators, name='calculators'),
